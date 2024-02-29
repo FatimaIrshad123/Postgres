6fa0042d0655651b736de155f102f83e3f90a7f1
FatimaIrshad <bibimemoona2017@gmail.com>
2024-02-29 10:55:45 +0500

    Added postgres practice code

---
+++ create-table.ts
@@ -0,0 +1,40 @@
+import { Client } from 'pg';
+const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'user12',
+    user: 'postgres',
+    password: 'fatima',
+  });
+  async function createTable() {
+    const createUserTableQuery = `
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL
+        );
+    `;
+
+    const client = await Client();
+
+    await client.query(createUserTableQuery);
+
+    const createTodosQuery = `
+        CREATE TABLE todos (
+            id SERIAL PRIMARY KEY,
+            title TEXT NOT NULL,
+            description TEXT,
+            user_id INTEGER REFERENCES users(id),
+            done BOOLEAN DEFAULT FALSE
+        );
+    `;
+
+
+    await client.query(createTodosQuery);
+
+    console.log("Table created successfully!");
+}
+
+
+
+createTable();
